211baec241e926693dfd91b1d689b6fd0d67e26a
bestcarBJ <best_car@outlook.fr>
2021-03-07 16:14:54 +0100

    Hello

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,15 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+Route::group(['prefix' => 'admin'], function () {
+    Voyager::routes();
+});
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
